af04cf0a5b0c13fa3ab4d3061aece5486535fd60
ernesthighlife-art <ernest.highlife@gmail.com>
2026-09-07 16:06:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hardware-inventory-v20260907-9';
+const CACHE='hardware-inventory-v20260907-11';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
