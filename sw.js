56f7ad168ab23b5a8a8d6f0e0b9278e92db1537f
ernesthighlife-art <ernest.highlife@gmail.com>
2026-09-07 16:20:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
-const CACHE='hardware-inventory-v20260907-11';
-const CORE=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(cache=>cache.put(r,cp));return res}).catch(()=>caches.match('./index.html'))))});
+const CACHE = 'inventory-v5';
+const APP_SHELL = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if(req.method !== 'GET') return;
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+    return res;
+  }).catch(()=>cached)));
+});
